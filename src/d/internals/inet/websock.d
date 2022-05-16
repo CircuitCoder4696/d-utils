be86c5f66394bcf870306d187c4aa3fc834aabc3
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2022-05-16 10:18:39 -0500

    Update websock.d

--- src/d/internals/inet/websock.d
+++ src/d/internals/inet/websock.d
@@ -4,10 +4,12 @@ private struct wsReqHeaderParser {
     import std.base64;
     import std.digest.sha;
     import std.string;
+    public string Connection;
     public string Host;
     public string Upgrade;
-    public string Connection;
+    public string Origin;
     public string Sec_WebSocket_Key;
+    public string Sec_WebSocket_Protocol;
     public string Sec_WebSocket_Version;
     public bool WebSocket_GetRequest;
     public this(string requestHeader) {
@@ -17,6 +19,8 @@ private struct wsReqHeaderParser {
             if(shd[0]=="Host")this.Host= shd[1];
             if(shd[0]=="Upgrade")this.Upgrade= shd[1];
             if(shd[0]=="Connection")this.Connection= shd[1];
+            if(shd[0]=="Sec_WebSocket_Protocol")this.Sec_WebSocket_Protocol= shd[1];
+            if(shd[0]=="Origin")this.Origin= shd[1];
             if(shd[0]=="Sec-WebSocket-Key")this.Sec_WebSocket_Key= shd[1];
             if(shd[0]=="Sec-WebSocket-Version")this.Sec_WebSocket_Version= shd[1];
         } else {
@@ -35,6 +39,7 @@ private struct wsResHeaderParser {
     public string Upgrade;
     public string Connection;
     public string Sec_WebSocket_Accept;
+    public string Sec_WebSocket_Protocol;
     public bool WebSocket_HTTPResponse;
     public this(string requestHeader) {
         string[] shd;
@@ -43,6 +48,7 @@ private struct wsResHeaderParser {
             if(shd[0]=="Upgrade")this.Upgrade= shd[1];
             if(shd[0]=="Connection")this.Connection= shd[1];
             if(shd[0]=="Sec-WebSocket-Accept")this.Sec_WebSocket_Accept= shd[1];
+            if(shd[0]=="Sec-WebSocket-Protocol")this.Sec_WebSocket_ Protocol = shd[1];
         } else {
             if(subHeader=="HTTP/1.1 101 Switching Protocols")this.WebSocket_HTTPResponse= true;
         };
