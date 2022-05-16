0611e5fb394e72787f253b512bb6548cadf3775e
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2022-05-16 09:05:42 -0500

    Basic web-socket request parser
    Added basic web-socket request header parser.

---
+++ src/d/internals/inet/websock.d
@@ -0,0 +1,24 @@
+module d.internals.inet.websock;
+
+public struct wsReqHeader {
+    import std.string;
+    public string Host;
+    public string Upgrade;
+    public string Connection;
+    public string Sec_WebSocket_Key;
+    public string Sec_WebSocket_Version;
+    public bool WebSocket_GetRequest;
+    public this(string requestHeader) {
+        string[] shd;
+        foreach(subHeader; requestHeader.split("\n"))if(subHeader.indexOf(": ") !=-1) {
+            shd= subHeader.split(": ");
+            if(shd[0]=="Host")this.Host= shd[1];
+            if(shd[0]=="Upgrade")this.Upgrade= shd[1];
+            if(shd[0]=="Connection")this.Connection= shd[1];
+            if(shd[0]=="Sec-WebSocket-Key")this.Sec_WebSocket_Key= shd[1];
+            if(shd[0]=="Sec-WebSocket-Version")this.Sec_WebSocket_Version= shd[1];
+        } else {
+            if(subHeader=="GET /chat HTTP/1.1")this.WebSocket_GetRequest= true;
+        };
+    };
+};
