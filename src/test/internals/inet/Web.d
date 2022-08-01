3212b3541001c0899695ab74900cc307d5f34d03
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-08-01 01:07:43 +0000

    Not done.

--- src/test/internals/inet/Web.d
+++ src/test/internals/inet/Web.d
@@ -1,5 +1,10 @@
 module test.internals.inet.Web;
 
 public class t_Web {
-    public void generate_http_
-}
+    public void generate_http_req_packet(string domainName, string path, string query) {
+        serial.
+    };
+    public void test(string[] ArgV) {
+        generate_http_req_packet("google.com", null, null);
+    };
+};
