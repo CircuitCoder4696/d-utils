6b3e82847102b98048ae7ad58e9b1a9517fb9a3d
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2022-08-01 20:11:24 +0000

    Documentation added.

--- src/d/reflection.d
+++ src/d/reflection.d
@@ -1,7 +1,15 @@
 module d.reflection;
 import core.runtime:defaultTraceHandler;
 
-public string[] stack_trace() {
+public string[] stack_trace() {   /++
+    #var d.reflection.$(methodName):$(methodType)
+    $div id="$(modulePath)" (
+        $h2(Reflection $(methodName))
+        $p(
+            $b($(methodName)) is supposed to provide un-processed information from the stack-trace.  This information can later be used to help point at a specific piece of code where information has been logged from.  
+        )
+    )
+++/
     version(GDC) {
         import std.stdio:writeln;
         writeln("[Err] ",__MODULE__," @",__LINE__,":   Reflection may require a lot of rewriting for it to work on `gdc`.  Try using `dmd`.  ");
